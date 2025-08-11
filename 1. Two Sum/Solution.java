e850fb27fdb3f7c564e0c9e27997e964302c3a5b
ArpitRajan <89370406+ArpitRajan@users.noreply.github.com>
2025-08-11 23:52:39 +0530

    Java Solution
          Submission Statistics:
          Question #: 1
          Language: Java
          Runtime: 46 ms
          Runtime percentile: 15.97
          Memory: 44.848 MB
          Memory percentile: 79.11

---
+++ 1. Two Sum/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int x=0,y=0;
+        one:
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    System.out.println((nums[i]+nums[j]));
+                    x=i;
+                    y=j;
+                    break one;
+                }
+            }
+        }
+        return new int[] {x,y};
+    }
+}
